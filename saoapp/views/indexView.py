a48366007b337b9f3bedd0a9e175a0daa535beca
Bruno Miguel Morais <bm.morais21@gmail.com>
2017-06-08 22:25:31 -0300

    projeto montado / index

---
+++ saoapp/views/indexView.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+from django.shortcuts import render
+from django.views.generic.base import View
+
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'index.html')
+
+    # def post(self, request):
+    #     form = UsuarioForm(request.POST)
+    #     if request.POST['password'] == request.POST['senha_conferir']:
+    #         if form.is_valid():
+    #             form.save()
+    #             user = User.objects.get(username=request.POST['username'])
+    #             user.save()
+    #             return render(request, 'index.html', {'cadastro': True, 'form': UsuarioForm()})
+    #         else:
+    #             return render(request, 'index.html', {'form': form, 'erro_cad': True})
+    #     else:
+    #         return render(request, 'index.html', {'form': form, 'senha_erro': True, 'erro_cad': True})
