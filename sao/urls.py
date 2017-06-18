1791d7823eb3970e00f4f028eb3b08689c2e2823
Bruno Miguel Morais <bm.morais21@gmail.com>
2017-06-18 17:41:08 -0300

    urls login/logout

--- sao/urls.py
+++ sao/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 from saoapp.views import *
@@ -22,8 +23,8 @@ from saoapp.views import *
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'logout.html'}, name='logout'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'logout.html'}, name='logout'),
 
     url(r'^$', IndexView.as_view(), name='index'),
 
