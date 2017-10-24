22ad9f9fc3a0dd68815696053aeaca75c381cdef
Bruno Miguel Morais <bm.morais21@gmail.com>
2017-10-24 14:40:31 -0200

    generic class based view

--- saoapp/views/disciplina_view.py
+++ saoapp/views/disciplina_view.py
@@ -2,7 +2,6 @@
 """View de disciplina"""
 
 from django.contrib.auth.decorators import login_required
-# from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, redirect
 from django.utils.decorators import method_decorator
 from django.views.generic.base import View
@@ -11,36 +10,16 @@ from django.views.generic import ListView
 from saoapp.forms.disciplina_form import DisciplinaForm
 from saoapp.models import DisciplinaModel
 
-
-# class DisciplinaListarView(View):
-#     """Classe de view de listagem de disciplinas"""
-#
-#     @method_decorator(login_required(login_url='/login/'))
-#     def get(self, request):
-#         """Método GET"""
-#
-#         if request.user.is_superuser:
-#             dc = DisciplinaModel.objects.all()
-#             paginator = Paginator(dc, 5)
-#             page = request.GET.get('page')
-#             try:
-#                 dados = paginator.page(page)
-#             except PageNotAnInteger:
-#                 dados = paginator.page(1)
-#             except EmptyPage:
-#                 dados = paginator.page(paginator.num_pages)
-#             return render(request, 'disciplina/listar.html', {'dados': dados})
-#         else:
-#             return redirect('index')
-
 class DisciplinaListarView(ListView):
     """Classe genérica de view de listagem de disciplinas"""
 
-    # TODO: terminar generic view
     template_name = 'disciplina/listar.html'
+    paginate_by = 5
 
     def get_queryset(self):
+        """Método de definição de queryset"""
         return DisciplinaModel.objects.all()
+
 
 
 class DisciplinaCadastrarView(View):
