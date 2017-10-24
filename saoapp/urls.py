c928b34fb0411043cf3a91448acc7c7a7544e79d
Bruno Miguel Morais <bm.morais21@gmail.com>
2017-10-24 13:13:07 -0200

    correção urls

--- saoapp/urls.py
+++ saoapp/urls.py
@@ -18,28 +18,28 @@ urlpatterns = [
 
     url(r'^aluno/$', AlunoListarView.as_view(), name='aluno_listar'),
     url(r'^aluno/cadastrar/$', AlunoCadastrarView.as_view(), name='aluno_cadastrar'),
-    url(r'^aluno/editar/(?P<id>\d+)/$', AlunoEditarView.as_view(), name='aluno_editar'),
-    url(r'^aluno/ocultar/(?P<id>\d+)/$', AlunoOcultarView.as_view(), name='aluno_ocultar'),
+    url(r'^aluno/editar/(?P<aluno_id>\d+)/$', AlunoEditarView.as_view(), name='aluno_editar'),
+    url(r'^aluno/ocultar/(?P<aluno_id>\d+)/$', AlunoOcultarView.as_view(), name='aluno_ocultar'),
 
     url(r'^ocorrencia/$', OcorrenciaListarView.as_view(), name='ocorrencia_listar'),
     url(r'^ocorrencia/cadastrar/$', OcorrenciaCadastrarView.as_view(), name='ocorrencia_cadastrar'),
-    url(r'^ocorrencia/editar/(?P<id>\d+)/$', OcorrenciaEditarView.as_view(), name='ocorrencia_editar'),
-    url(r'^ocorrencia/ocultar/(?P<id>\d+)/$', OcorrenciaOcultarView.as_view(), name='ocorrencia_ocultar'),
-    url(r'^ocorrencia/relatorio/(?P<id>\d+)/$', OcorrenciaRelatorioView.as_view(), name='ocorrencia_relatorio'),
+    url(r'^ocorrencia/editar/(?P<ocorrencia_id>\d+)/$', OcorrenciaEditarView.as_view(), name='ocorrencia_editar'),
+    url(r'^ocorrencia/ocultar/(?P<ocorrencia_id>\d+)/$', OcorrenciaOcultarView.as_view(), name='ocorrencia_ocultar'),
+    url(r'^ocorrencia/relatorio/(?P<ocorrencia_id>\d+)/$', OcorrenciaRelatorioView.as_view(), name='ocorrencia_relatorio'),
 
     url(r'^turma/$', TurmaListarView.as_view(), name='turma_listar'),
     url(r'^turma/cadastrar/$', TurmaCadastrarView.as_view(), name='turma_cadastrar'),
-    url(r'^turma/editar/(?P<id>\d+)/$', TurmaEditarView.as_view(), name='turma_editar'),
-    url(r'^turma/ocultar/(?P<id>\d+)/$', TurmaOcultarView.as_view(), name='turma_ocultar'),
+    url(r'^turma/editar/(?P<turma_id>\d+)/$', TurmaEditarView.as_view(), name='turma_editar'),
+    url(r'^turma/ocultar/(?P<turma_id>\d+)/$', TurmaOcultarView.as_view(), name='turma_ocultar'),
 
     url(r'^disciplina/$', DisciplinaListarView.as_view(), name='disciplina_listar'),
     url(r'^disciplina/cadastrar/$', DisciplinaCadastrarView.as_view(), name='disciplina_cadastrar'),
-    url(r'^disciplina/editar/(?P<id>\d+)/$', DisciplinaEditarView.as_view(), name='disciplina_editar'),
-    url(r'^disciplina/ocultar/(?P<id>\d+)/$', DisciplinaOcultarView.as_view(), name='disciplina_ocultar'),
+    url(r'^disciplina/editar/(?P<disciplina_id>\d+)/$', DisciplinaEditarView.as_view(), name='disciplina_editar'),
+    url(r'^disciplina/ocultar/(?P<disciplina_id>\d+)/$', DisciplinaOcultarView.as_view(), name='disciplina_ocultar'),
 
     url(r'^professor/$', ProfessorListarView.as_view(), name='professor_listar'),
     url(r'^professor/cadastrar/$', ProfessorCadastrarView.as_view(), name='professor_cadastrar'),
-    url(r'^professor/editar/(?P<id>\d+)/$', ProfessorEditarView.as_view(), name='professor_editar'),
-    url(r'^professor/ocultar/(?P<id>\d+)/$', ProfessorOcultarView.as_view(), name='professor_ocultar'),
+    url(r'^professor/editar/(?P<professor_id>\d+)/$', ProfessorEditarView.as_view(), name='professor_editar'),
+    url(r'^professor/ocultar/(?P<professor_id>\d+)/$', ProfessorOcultarView.as_view(), name='professor_ocultar'),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
